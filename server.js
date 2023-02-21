14d782696f4d35d905fdb455a788cac4efa0328b
Emmanuel-9 <oregadaniel181@gmail.com>
2023-02-21 11:02:22 +0300

    Change the format od tools in JenkinsFile.

--- server.js
+++ server.js
@@ -1,50 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const path = require("path");
-dotenv.config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
 // Define routes
-let index = require("./routes/index");
-let image = require("./routes/image");
-const config = require("./_config");
+let index = require('./routes/index');
+let image = require('./routes/image');
+
+// connecting the database
+// let mongodb_url = 'mongodb://localhost:27017/';
+// let dbName = 'darkroom';
+mongoose.connect(`mongodb+srv://oregano:.Sct.111@cluster0.ip5exgc.mongodb.net/?retryWrites=true&w=majority/Cluster0`, { useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+    if (err) console.log(err)
+});
+
+// test if the database has connected successfully
+let db = mongoose.connection;
+db.once('open', ()=>{
+    console.log('Database connected successfully')
+})
 
 // Initializing the app
 const app = express();
 
-// connecting the database
-async function connectdb() {
-  await mongoose
-    .connect(config.mongoURI.development, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .catch((err) => {
-      console.log("Error connecting to the database. \n" + err);
-    });
-}
-
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once("open", () => {
-  console.log("Database connected successfully");
-});
 
 // View Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Set up the public folder;
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json());
+app.use(express.json())
 
-app.use("/", index);
-app.use("/image", image);
 
+app.use('/', index);
+app.use('/image', image);
+
+
+
+ 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
-  await connectdb();
+app.listen(PORT,() =>{
+    console.log(`Server is listening at http://localhost:${PORT}`)
 });
